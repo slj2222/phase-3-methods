99be510a8a6255424f1a76625b5cc2303a9469c1
Spencer Johnson <sljohnson2222@gmail.com>
2022-03-14 00:58:17 -0500

    commit tree db6dffdce1055b132d30fa7785a6b0b68b3dcaec

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name 
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+greet_with_default "Naureen"
+
+def add num1, num2
+     num1 + num2
+end
+
+add 2, 5
+
+# def add_other num
+#     num
+# end 
+# add_other 2, 5
+
+def halve num
+    return nil unless num.class == Integer
+    num / 2
+end
